0c8abf725182ed2ee32e4c60695f0707d4e253cc
kobeomseok95 <kobumssh@naver.com>
2025-12-20 13:16:27 +0900

    leetcode 673 풀이
    - brute force + 재귀 방식으로 풀이

---
+++ src/leetcode/medium/673.kt
@@ -0,0 +1,63 @@
+package leetcode.medium
+
+/**
+ * Given an integer array nums, return the number of longest increasing subsequences.
+ *
+ * Notice that the sequence has to be strictly increasing.
+ *
+ * Example 1:
+ * Input: nums = [1,3,5,4,7]
+ * Output: 2
+ * Explanation: The two longest increasing subsequences are [1, 3, 4, 7] and [1, 3, 5, 7].
+ *
+ * Example 2:
+ * Input: nums = [2,2,2,2,2]
+ * Output: 5
+ * Explanation: The length of the longest increasing subsequence is 1, and there are 5 increasing subsequences of length 1, so output 5.
+ *
+ * Constraints:
+ * 1 <= nums.length <= 2000
+ * -10^6 <= nums[i] <= 10^6
+ * The answer is guaranteed to fit inside a 32-bit integer.
+ *
+ * Longest Increasing Subsequence: 원소가 n개인 배열의 일부 원소를 골라내서 만든 부분 수열 중, 각 원소가 이전 보다 크다는 조건을 만족하고 그 길이가 최대인 부분 수열
+ *
+ * 접근법: Brute Force 로 접근
+ *
+ * 시간 복잡도 : O(2^n * n)
+ * 공간 복잡도 : O(2^n * n)
+ */
+class `673` {
+    fun findNumberOfLIS(nums: IntArray): Int {
+        val allSubsequences = mutableListOf<List<Int>>()
+        generateSubsequences(nums, 0, mutableListOf(), allSubsequences)
+        val increasingOnes = allSubsequences.filter { isIncreasing(it) }
+        val maxLength = increasingOnes.maxOfOrNull { it.size } ?: 0
+        return increasingOnes.count { it.size == maxLength }
+    }
+
+    private fun generateSubsequences(
+        nums: IntArray,
+        index: Int,
+        current: MutableList<Int>,
+        allSubsequences: MutableList<List<Int>>
+    ) {
+        if (index == nums.size) {
+            allSubsequences.add(current.toList())
+            return
+        }
+        generateSubsequences(nums, index + 1, current, allSubsequences)
+        current.add(nums[index])
+        generateSubsequences(nums, index + 1, current, allSubsequences)
+        current.removeAt(current.lastIndex)
+    }
+
+    private fun isIncreasing(list: List<Int>): Boolean {
+        if (list.size <= 1) return true
+
+        for (i in 1 until list.size) {
+            if (list[i] <= list[i - 1]) return false
+        }
+        return true
+    }
+}
