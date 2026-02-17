671237eebcd936b9f143b2d3ff03c1a6eed3fffe
kobeomseok95 <kobumssh@naver.com>
2026-02-17 12:41:46 +0900

    leetcode 62 풀이
    - 1D DP 풀이로 공간 복잡도 개선

---
+++ src/leetcode/medium/62_2.kt
@@ -0,0 +1,32 @@
+package leetcode.medium
+
+/**
+ * There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+ * Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+ * The test cases are generated so that the answer will be less than or equal to 2 * 109.
+ *
+ * Constraints:
+ * - 1 <= m, n <= 100
+ *
+ * 시간 복잡도 : O(m * n)
+ * 공간 복잡도 : O(minOf(m, n))
+ */
+class `62_2` {
+    fun uniquePaths(m: Int, n: Int): Int {
+        val size = minOf(m, n)
+        val repeat = maxOf(m, n)
+        val dp = IntArray(size) { 1 }
+        repeat(repeat - 1) {
+            (1 until size).forEach { i ->
+                dp[i] += dp[i - 1]
+            }
+        }
+        return dp[size - 1]
+    }
+}
+
+fun main() {
+    val solution = `62_2`()
+    println(solution.uniquePaths(m = 3, n = 7)) // 28
+    println(solution.uniquePaths(m = 3, n = 2)) // 3
+}
