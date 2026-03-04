a684df83d21bc042458e48618a93316b46bbb70f
kobeomseok95 <kobumssh@naver.com>
2026-03-04 13:07:42 +0900

    leetcode 973 풀이
    - 정렬로 접근

--- src/leetcode/medium/973.kt
+++ src/leetcode/medium/973.kt
@@ -12,12 +12,17 @@ package leetcode.medium
  * - 1 <= k <= points.length <= 10^4
  * - -10^4 <= xi, yi <= 10^4
  *
- * 시간 복잡도: TODO("")
- * 공간 복잡도: TODO("")
+ * 시간 복잡도: O(n * log n)
+ * 공간 복잡도: O(k)
  */
 class `973` {
     fun kClosest(points: Array<IntArray>, k: Int): Array<IntArray> {
-        TODO("")
+        points.sortBy { getDistance(it) }
+        return points.take(k).toTypedArray()
+    }
+
+    private fun getDistance(point: IntArray): Int {
+        return point.first() * point.first() + point.last() * point.last()
     }
 }
 
