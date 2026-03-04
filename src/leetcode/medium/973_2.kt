d9a78676a8ae0a4eb4a2740db2c9d9266a0d3963
kobeomseok95 <kobumssh@naver.com>
2026-03-04 13:34:50 +0900

    leetcode 973 풀이
    - PriorityQueue 활용

---
+++ src/leetcode/medium/973_2.kt
@@ -0,0 +1,43 @@
+package leetcode.medium
+
+import java.util.PriorityQueue
+
+/**
+ * Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane and an integer k,
+ * return the k closest points to the origin (0, 0).
+ *
+ * The distance between two points on the X-Y plane is the Euclidean distance (i.e., √(x1 - x2)² + (y1 - y2)²).
+ *
+ * You may return the answer in any order. The answer is guaranteed to be unique (except for the order that it is in).
+ *
+ * Constraints:
+ * - 1 <= k <= points.length <= 10^4
+ * - -10^4 <= xi, yi <= 10^4
+ *
+ * 시간 복잡도: O(n * log k)
+ * 공간 복잡도: O(k)
+ */
+class `973_2` {
+    fun kClosest(points: Array<IntArray>, k: Int): Array<IntArray> {
+        val priorityQueue = PriorityQueue<IntArray> { o1, o2 ->
+            getDistance(o2) - getDistance(o1)
+        }
+        points.forEach { point ->
+            priorityQueue.add(point)
+            if (priorityQueue.size > k) {
+                priorityQueue.poll()
+            }
+        }
+        return priorityQueue.map { it }.toTypedArray()
+    }
+
+    private fun getDistance(point: IntArray): Int {
+        return point.first() * point.first() + point.last() * point.last()
+    }
+}
+
+fun main() {
+    val solution = `973_2`()
+    println(solution.kClosest(arrayOf(intArrayOf(1, 3), intArrayOf(-2, 2)), 1).contentDeepToString()) // [[-2,2]]
+    println(solution.kClosest(arrayOf(intArrayOf(3, 3), intArrayOf(5, -1), intArrayOf(-2, 4)), 2).contentDeepToString()) // [[3,3],[-2,4]]
+}
